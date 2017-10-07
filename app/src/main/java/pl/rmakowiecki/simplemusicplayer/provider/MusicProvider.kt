ed0d9d10d254bceb8cbe8531844728376a33c23a
Haydart <makowiecki.radoslaw@gmail.com>
2017-10-07 23:56:51 +0200

    Lazy initialize songs & albums extracted from provider

--- app/src/main/java/pl/rmakowiecki/simplemusicplayer/provider/MusicProvider.kt
+++ app/src/main/java/pl/rmakowiecki/simplemusicplayer/provider/MusicProvider.kt
@@ -15,12 +15,12 @@ import java.util.*
 const val ALBUM_COVER_DIRECTORY = "content://media/external/audio/albumart"
 
 class MusicProvider(private val context: Context) {
-    var songList = mutableListOf<Song>()
-        private set
-    var albumList = mutableListOf<Album>()
-        private set
+    val songList by lazy { retrieveDeviceSongList() }
 
-    private fun retrieveDeviceSongList() {
+    val albumList by lazy { retrieveDeviceAlbumList() }
+
+    private fun retrieveDeviceSongList(): MutableList<Song> {
+        val list = mutableListOf<Song>()
         val musicResolver = context.contentResolver
         musicResolver.query(EXTERNAL_CONTENT_URI, null, null, null, null).use {
             if (it != null && it.moveToFirst()) {
@@ -39,17 +39,25 @@ class MusicProvider(private val context: Context) {
                     val albumCoverUriPath = Uri.parse(ALBUM_COVER_DIRECTORY)
                     val albumArtUri = ContentUris.withAppendedId(albumCoverUriPath, albumCoverId)
                     val songDuration = it.getLong(durationColumn)
-                    songList.add(Song(thisId, songTitle, songArtist, songAlbum, albumArtUri, songDuration.toInt()))
+                    list.add(Song(thisId, songTitle, songArtist, songAlbum, albumArtUri, songDuration.toInt()))
                 } while (it.moveToNext())
             }
         }
-        return Collections.sort(songList) { a, b -> a.title.compareTo(b.title) }
+
+        Collections.sort(list) { a, b -> a.title.compareTo(b.title) }
+        return list
     }
 
-    private fun retrieveDeviceAlbumList() {
-        albumList = ArrayList<Album>()
+    private fun retrieveDeviceAlbumList(): MutableList<Album> {
+        val list = mutableListOf<Album>()
         val musicResolver = context.contentResolver
-        val projection = arrayOf(MediaStore.Audio.Albums._ID, MediaStore.Audio.Albums.ALBUM, MediaStore.Audio.Albums.ARTIST, MediaStore.Audio.Albums.ALBUM_ART, MediaStore.Audio.Albums.NUMBER_OF_SONGS)
+        val projection = arrayOf(
+                MediaStore.Audio.Albums._ID,
+                MediaStore.Audio.Albums.ALBUM,
+                MediaStore.Audio.Albums.ARTIST,
+                MediaStore.Audio.Albums.ALBUM_ART,
+                MediaStore.Audio.Albums.NUMBER_OF_SONGS
+        )
         val sortOrder = ALBUM + " COLLATE NOCASE ASC"
         musicResolver.query(MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI, projection, null, null, sortOrder).use {
             if (it != null && it.moveToFirst()) {
@@ -62,13 +70,14 @@ class MusicProvider(private val context: Context) {
                     val artist = it.getString(artistColumn)
                     val albumCoverUriPath = Uri.parse(ALBUM_COVER_DIRECTORY)
                     val albumArtUri = ContentUris.withAppendedId(albumCoverUriPath, id)
-                    albumList.add(Album(id, name, artist, ArrayList<Song>(), albumArtUri))
+                    list.add(Album(id, name, artist, ArrayList<Song>(), albumArtUri))
                 } while (it.moveToNext())
             }
         }
 
-        Collections.sort(albumList) { a, b -> a.artist.compareTo(b.artist) }
+        Collections.sort(list) { a, b -> a.artist.compareTo(b.artist) }
         matchSongsWithAlbums()
+        return list
     }
 
 
